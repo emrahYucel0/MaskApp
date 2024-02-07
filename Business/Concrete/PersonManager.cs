b8e423c3c6ccaf7e7ba6cad5632ad6b7d8aed4c1
Emrah Yücel <127504239+emrahYucel0@users.noreply.github.com>
2024-02-07 13:44:18 +0300

    Add project files.

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using Entities.Concretes;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete;
+
+public class PersonManager : IApplicantService
+{
+    public void ApplyForMask(Person person)
+    {
+        
+    }
+
+    public bool CheckPerson(Person person)
+    {
+        KPSPublicSoapClient client = new(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+
+        return client.TCKimlikNoDogrulaAsync(
+                    person.NationalIdentity,
+                    person.FirstName,
+                    person.LastName,
+                    person.DateOfBirthYear).Result.Body.TCKimlikNoDogrulaResult;
+    }
+
+
+    public List<Person> getList()
+    {
+        return null;
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,12 @@
+﻿using Business.Concrete;
+using Entities.Concretes;
+
+Person person = new();
+
+person.FirstName = "Emrah";
+person.LastName = "Yücel";
+person.DateOfBirthYear = 1985;
+person.NationalIdentity = 11223344556;
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person);
